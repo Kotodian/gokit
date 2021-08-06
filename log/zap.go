2ae0c857cac052c35cf1240b0e6343a2e84274bf
linqiankai <417350372@qq.com>
2021-08-06 10:41:21 +0800

    完善zap

--- log/zap.go
+++ log/zap.go
@@ -29,14 +29,50 @@ func Info(format string, fields ...zap.Field) {
 	logger.Info(format, fields...)
 }
 
+func Infof(format string, args ...interface{}) {
+	logger.Sugar().Infof(format, args...)
+}
+
 func Debug(format string, fields ...zap.Field) {
 	logger.Debug(format, fields...)
+}
+
+func Debugf(format string, args ...interface{}) {
+	logger.Sugar().Debugf(format, args...)
 }
 
 func Warn(format string, fields ...zap.Field) {
 	logger.Warn(format, fields...)
 }
 
+func Warnf(format string, args ...interface{}) {
+	logger.Sugar().Warnf(format, args...)
+}
+
 func Error(format string, fields ...zap.Field) {
 	logger.Error(format, fields...)
+}
+
+func Errorf(format string, args ...interface{}) {
+	logger.Sugar().Errorf(format, args...)
+}
+
+func String(key, value string) zap.Field {
+	return zap.String(key, value)
+}
+
+func Float64(key string, value float64) zap.Field {
+	return zap.Float64(key, value)
+}
+
+func Int64(key string, value int64) zap.Field {
+	return zap.Int64(key, value)
+}
+
+func Int(key string, value int) zap.Field {
+	return zap.Int(key, value)
+}
+
+func Int32(key string, value int32) zap.Field {
+	return zap.Int32(key, value)
 }
