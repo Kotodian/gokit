df1b9cd7ccada2b60c86a1339fd554380cbc9622
linqiankai <417350372@qq.com>
2021-09-06 10:12:51 +0800

    elasticsearch封装

--- datasource/elasticsearch/init.go
+++ datasource/elasticsearch/init.go
@@ -2,89 +2,103 @@ package elasticsearch
 
 import (
 	"context"
-	"time"
-
 	"github.com/olivere/elastic/v7"
-	"github.com/silenceper/pool"
+	"time"
 )
 
-var p pool.Pool
+var client *elastic.Client
 
 func init() {
-	//factory 创建连接的方法
-	factory := func() (interface{}, error) {
-		return elastic.NewClient(elastic.SetSniff(false), elastic.SetHealthcheck(false), elastic.SetURL("http://elasticsearch-master:9200"))
-	}
-
-	//close 关闭链接的方法
-	_close := func(v interface{}) error {
-		return nil
-	}
-
-	//创建一个连接池： 初始化5，最大链接30
-	poolConfig := &pool.Config{
-		InitialCap: 5,
-		MaxIdle:    10,
-		MaxCap:     65535,
-		Factory:    factory,
-		Close:      _close,
-		//链接最大空闲时间，超过该时间的链接 将会关闭，可避免空闲时链接EOF，自动失效的问题
-		IdleTimeout: 30 * time.Second,
-	}
-	// ch = make(chan struct{}, 1)
-
-	// go func() {
 	var err error
-	p, err = pool.NewChannelPool(poolConfig)
+	client, err = elastic.NewClient(
+		elastic.SetSniff(false),
+		elastic.SetHealthcheck(true),
+		elastic.SetHealthcheckTimeout(5*time.Second),
+		elastic.SetURL("http://elasticsearch-master:9200"),
+		elastic.SetRetrier(elastic.NewBackoffRetrier(elastic.NewSimpleBackoff(200))),
+	)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func Conn() (*elastic.Client, error) {
-	c, err := p.Get()
+func IndexCreate(ctx context.Context, index string) error {
+	_, err := client.CreateIndex(index).Do(ctx)
+	return err
+}
+
+func IndexExists(ctx context.Context, index string) (bool, error) {
+	exists, err := client.IndexExists(index).Do(ctx)
 	if err != nil {
-		return nil, err
+		return false, err
 	}
-	return c.(*elastic.Client), nil
+	return exists, nil
 }
 
-func Close(c interface{}) error {
-	return p.Put(c)
-}
-
-func Update(ctx context.Context, index string, _type string, id string, fields map[string]interface{}, defaultValue ...map[string]interface{}) error {
-	es, err := Conn()
+func Insert(ctx context.Context, id, index string, body interface{}) error {
+	indexService := client.Index().Index(index).BodyJson(body)
+	if id != "" {
+		indexService.Id(id)
+	}
+	_, err := indexService.Do(ctx)
 	if err != nil {
 		return err
 	}
-	defer Close(es)
+	_, err = client.Refresh(index).Do(ctx)
+	return err
+}
 
-	//
-	//scriptArr := []string{}
-	//for k, v := range fields {
-	//	if _, ok := v.(string); ok {
-	//		scriptArr = append(scriptArr, fmt.Sprintf("ctx._source.%s='%v'", k, v))
-	//	} else {
-	//		scriptArr = append(scriptArr, fmt.Sprintf("ctx._source.%s=%v", k, v))
-	//	}
-	//}
-	//
-	//fmt.Println(scriptArr)
-	//_, err = es.Update().Index(index).Type(_type).Id(id).Script(
-	//	elastic.NewScriptInline(strings.Join(scriptArr, ";")),
-	//).Upsert(defaultValue).Do(ctx)
-	//
-	up := es.Update().Index(index).Type(_type).Id(id).Doc(fields)
+func InsertBulk(ctx context.Context, index string, body []interface{}) error {
+	request := client.Bulk()
+	for _, v := range body {
+		tmp := v
+		req := elastic.NewBulkIndexRequest().Index(index).Doc(tmp)
+		request = request.Add(req)
+	}
+	_, err := request.Do(ctx)
+	return err
+}
+
+func Update(ctx context.Context, index string, id string, fields map[string]interface{}, defaultValue ...map[string]interface{}) error {
+	up := client.Update().Index(index).Id(id).Doc(fields)
 	if len(defaultValue) > 0 {
 		up.Upsert(defaultValue[0])
 	}
-	if _, err = up.Do(ctx); err != nil {
+	if _, err := up.Do(ctx); err != nil {
 		return err
 	}
-	_, err = es.Refresh(index).Do(ctx)
+	_, err := client.Refresh(index).Do(ctx)
 	if err != nil {
 		return err
 	}
 	return err
+}
+
+func UpdateBulk(ctx context.Context, es *elastic.Client, index string, body []map[string]string) error {
+	bulkRequest := es.Bulk()
+	for _, v := range body {
+		tmp := v
+		tmps := copyMap(tmp)
+		delete(tmps, "id")
+		req := elastic.NewBulkUpdateRequest().Index(index).Id(tmp["id"]).Doc(tmps)
+		bulkRequest = bulkRequest.Add(req)
+	}
+	_, err := bulkRequest.Do(ctx)
+	return err
+}
+
+func Delete(ctx context.Context, es *elastic.Client, index, id string) error {
+	_, err := es.Delete().Index(index).Id(id).Do(ctx)
+	if err != nil {
+		return err
+	}
+	_, err = client.Refresh(index).Do(ctx)
+	return err
+}
+func copyMap(m map[string]string) map[string]string {
+	cp := make(map[string]string)
+	for is, vs := range m {
+		cp[is] = vs
+	}
+	return cp
 }
