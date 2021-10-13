9d6784b4434ac6723c859d9f0aa08f4b80010feb
linqiankai <417350372@qq.com>
2021-10-13 09:46:06 +0800

    所有的init全由服务自己去使用去触发

--- id/lib.go
+++ id/lib.go
@@ -15,23 +15,21 @@ import (
 
 // var partnerId uint32
 
+func Init() {
+	// 创建 IdGeneratorOptions 对象，请在构造函数中输入 WorkerId：
+	var options = idgen.NewIdGeneratorOptions(1)
+	// options.WorkerIdBitLength = 10 // WorkerIdBitLength 默认值6，支持的 WorkerId 最大值为2^6-1，若 WorkerId 超过64，可设置更大的 WorkerIdBitLength
+	// ...... 其它参数设置参考 IdGeneratorOptions 定义，一般来说，只要再设置 WorkerIdBitLength （决定 WorkerId 的最大值）。
 
-func init() {
-// 创建 IdGeneratorOptions 对象，请在构造函数中输入 WorkerId：
-var options = idgen.NewIdGeneratorOptions(1)
-// options.WorkerIdBitLength = 10 // WorkerIdBitLength 默认值6，支持的 WorkerId 最大值为2^6-1，若 WorkerId 超过64，可设置更大的 WorkerIdBitLength
-// ...... 其它参数设置参考 IdGeneratorOptions 定义，一般来说，只要再设置 WorkerIdBitLength （决定 WorkerId 的最大值）。
-
-// 保存参数（必须的操作，否则以上设置都不能生效）：
-idgen.SetIdGenerator(options)
-// 以上初始化过程只需全局一次，且必须在第2步之前设置。
+	// 保存参数（必须的操作，否则以上设置都不能生效）：
+	idgen.SetIdGenerator(options)
+	// 以上初始化过程只需全局一次，且必须在第2步之前设置。
 }
 
 func Next() datasource.UUID {
 	newID := idgen.NextId()
 	return datasource.UUID(newID)
 }
-
 
 // func GetSnowFlake(businessId uint32) (*SnowFlake, error) {
 // 	var key uint64 = uint64(businessId) << SequenceBits
