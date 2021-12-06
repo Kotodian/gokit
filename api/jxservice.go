bfcae35e99997caecf115f37ffa5e6d9a41a6986
linqiankai <417350372@qq.com>
2021-12-06 09:33:24 +0800

    提供jx-service获取推送时间间隔

--- api/jxservice.go
+++ api/jxservice.go
@@ -3,15 +3,18 @@ package api
 import (
 	"bytes"
 	"encoding/json"
+	"github.com/Kotodian/gokit/datasource"
 	"io/ioutil"
 	"net/http"
 	"time"
 )
 
 type pushIntervalRequest struct {
+	EquipmentID datasource.UUID `json:"equipmentId"`
 }
 
 type pushIntervalResponse struct {
+	OrderPushInterval int `json:"orderPushInterval"`
 }
 
 func PushInterval(req *pushIntervalRequest) (*pushIntervalResponse, error) {
@@ -22,12 +25,11 @@ func PushInterval(req *pushIntervalRequest) (*pushIntervalResponse, error) {
 	client := http.DefaultClient
 	client.Timeout = 10 * time.Second
 	reader := bytes.NewReader(reqBytes)
-	resp, err := client.Post("http://jx-services:8080/", defaultContentType, reader)
+	resp, err := client.Post("http://jx-services:8080/equip/getEquipCallerPushOrderInterval", defaultContentType, reader)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
-
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
