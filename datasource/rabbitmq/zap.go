6f84b2d499523fa5b8a92e8400bef3ed63fd0970
linqiankai <417350372@qq.com>
2022-02-21 16:36:24 +0800

    zap rabbitmq hook

--- datasource/rabbitmq/zap.go
+++ datasource/rabbitmq/zap.go
@@ -1,5 +1,12 @@
 package rabbitmq
 
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+	"time"
+)
+
 type rabbitmqHook struct {
 	queue   string
 	service string
@@ -14,4 +21,46 @@ func NewRabbitmqHook(queue string, service string) *rabbitmqHook {
 
 func (r *rabbitmqHook) Write(p []byte) (n int, err error) {
 	return 0, nil
+}
+
+func NewEncoderConfig() zapcore.EncoderConfig {
+	return zapcore.EncoderConfig{
+		// Keys can be anything except the empty string.
+		TimeKey:        "date_time",
+		LevelKey:       "level",
+		NameKey:        "name",
+		CallerKey:      "caller",
+		MessageKey:     "message",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.CapitalLevelEncoder,
+		EncodeTime:     TimeEncoder,
+		EncodeDuration: zapcore.StringDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+}
+
+func TimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(t.Format("2006-01-02 15:04:05.000"))
+}
+
+func NewZapLogger(minLevel zapcore.Level, version, queue, service string) *zap.Logger {
+	writeSyncerList := make([]zapcore.WriteSyncer, 0)
+	coreList := make([]zapcore.Core, 0)
+
+	levelFunc := zap.LevelEnablerFunc(func(level zapcore.Level) bool {
+		return level >= minLevel
+	})
+	hook := NewRabbitmqHook(queue, service)
+	writeSyncerList = append(writeSyncerList, zapcore.AddSync(os.Stdout))
+	writeSyncerList = append(writeSyncerList, zapcore.AddSync(hook))
+	coreList = append(coreList, zapcore.NewCore(zapcore.NewJSONEncoder(NewEncoderConfig()), zapcore.NewMultiWriteSyncer(writeSyncerList...), levelFunc))
+	core := zapcore.NewTee(coreList...)
+	var logger *zap.Logger
+	if minLevel == zapcore.DebugLevel {
+		logger = zap.New(core, zap.Development(), zap.AddCaller(), zap.AddStacktrace(zap.ErrorLevel))
+	} else {
+		logger = zap.New(core)
+	}
+	return logger
 }
