c598c0e5b1753ff1e37b6e176c8b8ee88e9237be
linqiankai <417350372@qq.com>
2022-04-14 13:53:41 +0800

    cp56time2a解析时反转下

--- ac/lib/bytes_test.go
+++ ac/lib/bytes_test.go
@@ -8,6 +8,9 @@ import (
 
 func TestParseCP56Time2a(t *testing.T) {
 	b := []byte{0x50, 0x46, 0x24, 0x0d, 0xd3, 0x02, 0x14}
+	b = ReserveBytes(b)
+	t.Log(ParseCP56Time2a(b).Format("2006-01-02 15:04:05"))
+	b = []byte{0x15, 0x09, 0x01, 0x0A, 0x1F, 0xA1, 0xE3}
 	t.Log(ParseCP56Time2a(b).Format("2006-01-02 15:04:05"))
 }
 
